ee22b55ca581531e684fa3e62d69ede645131394
jvens <Jeramie.Vens@gmail.com>
2017-10-11 00:36:25 -0500

    added firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 var functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.bigben = functions.https.onRequest((req, res) => {
+	const hours = (new Date().getHours() % 12) + 1;
+	res.status(200).send(`<!doctype html>
+		<head>
+			<title>Time</title>
+		</head>
+		<body>
+			${'BONG '. repeat(hours)}<br>
+			req.method=${req.method}<br>
+			req.body.text=${req.body.text}
+		</body>
+		</html>`);
+});
+
+
+exports.event = functions.https.onRequest((req, res) => {
+	var data = "<html><body>";
+	switch (req.method) {
+		case "GET":
+			data += "GET";
+			break;
+		case "POST":
+			data += "POST";
+			//var d = JSON.parse(req.body);
+			//data += d;
+			data += req.body.name;
+			break;
+		case "DELETE":
+			data += "DELETE";
+			break;
+		case "PUT":
+			data += "PUT";
+			break;
+	}
+	data += JSON.stringify(req.body);
+	data += req.url;
+	data += req.params;
+	data += "</body></html>";
+	res.status(200).send(data);
+});
