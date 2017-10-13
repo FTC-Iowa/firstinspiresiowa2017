ebe6870e584d53a6222955ad3a3a7a200d9b3efe
jvens <Jeramie.Vens@gmail.com>
2017-10-12 23:13:03 -0500

    uploading matches and teams arrays

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const Firestore = require('@google-cloud/firestore');
+
+admin.initializeApp (functions.config().firebase);
+const firestore = new Firestore(functions.config().firebase)
+
 
 exports.bigben = functions.https.onRequest((req, res) => {
 	const hours = (new Date().getHours() % 12) + 1;
@@ -14,29 +20,54 @@ exports.bigben = functions.https.onRequest((req, res) => {
 		</html>`);
 });
 
+exports.change = functions.firestore.document('Teams/{teamID}').onWrite((event) => {
+	console.log("document changed!!!");
+})
 
-exports.event = functions.https.onRequest((req, res) => {
-	var data = "<html><body>";
-	switch (req.method) {
-		case "GET":
-			data += "GET";
-			break;
-		case "POST":
-			data += "POST";
-			//var d = JSON.parse(req.body);
-			//data += d;
-			data += req.body.name;
-			break;
-		case "DELETE":
-			data += "DELETE";
-			break;
-		case "PUT":
-			data += "PUT";
-			break;
+exports.matches = functions.https.onRequest((req, res) => {
+	var matches = req.body.matches;
+	if(Array.isArray(matches)){
+		matches.forEach( function (m) {	
+			var docname = "Events/" + m.event + "/Matches/" + m.name;
+			console.log(docname);
+			var doc = firestore.collection("Events").doc(m.event).collection("Matches").doc(m.name);
+			console.log(`Path of document is ${doc.path}`)
+			doc.set( {
+				number: m.number,
+				red: m.red,
+				blue: m.blue
+			}).then(() => {
+				console.log("done!");
+			}).catch((err) => {
+				console.log(`Failed to create document: ${err}`);
+			});
+		});
 	}
-	data += JSON.stringify(req.body);
-	data += req.url;
-	data += req.params;
-	data += "</body></html>";
-	res.status(200).send(data);
+	res.status(200).send("done!");
 });
+
+
+exports.teams = functions.https.onRequest((req, res) => {
+	//var ret = "Teams: "
+	var data = req.body;
+	if(Array.isArray(data.teams)) {
+		data.teams.forEach( function (i) {
+	//		ret += i.number + ", ";
+			var docname = 'Teams/' + i.number;
+			var doc = firestore.doc(docname);
+			doc.set({
+				Name: i.teamname,
+				School: i.school,
+				City: i.city,
+				State: i.state,
+				Country: i.country,
+			}).then(() => {
+				console.log("Create " + docname);
+			});
+			
+		})
+	}
+	res.status(200).send("done!");
+});
+
+
